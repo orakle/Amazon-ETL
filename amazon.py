60711594ad4a89600dc251e380ca393b39b4f90a
viktor <verkter@localhost.(none)>
2014-10-04 11:28:03 -0700

    Added amazon.py

---
+++ amazon.py
@@ -0,0 +1,54 @@
+import datetime
+import csv
+
+
+review_list = []
+
+# Read file into a list
+# Strip new lines
+for line in open('movies.sample.txt'):
+    review_list.append(line.strip())
+    
+# Grouping review into dict of dicts based on the empy string item
+grouped_reviews = []
+new_group = []
+for line in review_list:
+    if line != '':
+        new_group.append(line)
+    else:
+        grouped_reviews.append(new_group)
+        new_group = []
+
+# Create a list of dictionaries
+list_of_dict = []
+
+for review in grouped_reviews:
+    new_dict = {}
+    for item in review:
+        rev_key, rev_value = item.split(': ', 1)
+        clean_rev_key = rev_key.split('/')[1]
+        new_dict[clean_rev_key] = rev_value
+    list_of_dict.append(new_dict)
+
+# Convert time stamp into time
+# Convert score string into float
+for review in list_of_dict:
+    # Convert string into a float
+    review['score'] = float(review['score'])
+    
+    # Convert integer timestamp into year, month and date
+    time_float = float(review['time'])
+    temp_time = datetime.datetime.fromtimestamp(time_float)
+    format_time = str(temp_time.month) + ':' + str(temp_time.day) + ':' + str(temp_time.year)
+    review['time'] = format_time
+
+
+# Output to tsv
+# Create a writer object
+writer = csv.DictWriter(open('movie.sample.output.tsv', 'w'),
+                       ['productId', 'profileName', 'helpfulness', 'score', 'time'],
+                       delimiter = '\t',
+                       extrasaction = 'ignore')
+writer.writeheader()
+for review in list_of_dict:
+    writer.writerow(review)
